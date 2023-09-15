4b47623d7377cd6f21a04f39166256a908b6cece
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-15 13:56:57 +0000

    ex03

--- Python0/ex03/NULL_not_found.py
+++ Python0/ex03/NULL_not_found.py
@@ -1,1 +1,15 @@
 def NULL_not_found(object: any) -> int:
+    if object is None:
+        print(f"Nothing: {object}", type(None))
+    elif isinstance(object, float) and object != object:
+        print(f"Cheese: {object}", type(float("NaN")))
+    elif isinstance(object, bool) and not object:
+        print(f"Fake: {object}", type(object))
+    elif isinstance(object, int) and object == 0:
+        print(f"Zero: {object}", type(0))
+    elif isinstance(object, str) and not object:
+        print(f"Empty: {object}", type(''))
+    else:
+        print("Type not Found")
+        return 1
+    return 0
