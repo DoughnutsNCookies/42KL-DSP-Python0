9f98170332808fb0cbf41056ae63e7dc75b06fe4
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-16 14:32:53 +0000

    ex08

--- Python0/ex08/tester.py
+++ Python0/ex08/tester.py
@@ -2,12 +2,10 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-for elem in ft_tqdm(range(5)):
-    sleep(0.01)
-    sleep(1)
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
 print()
 
-for elem in tqdm(range(5)):
-    sleep(0.01)
-    sleep(1)
+for elem in tqdm(range(333)):
+    sleep(0.005)
 print()
