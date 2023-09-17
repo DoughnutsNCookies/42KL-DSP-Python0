bf8f7de57c56dd0e24d578a20a4cd0bbef4742c2
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-17 17:29:25 +0000

    ex09

--- Python0/ex09/tester.py
+++ Python0/ex09/tester.py
@@ -2,8 +2,8 @@ from schuahpackage import say_hello, add, sub, mul, div, mod
 
 # python setup.py sdist bdist_wheel
 say_hello()
-print(add(1,2))
-print(sub(1,2))
-print(mul(1,2))
-print(div(1,2))
-print(mod(1,2))
+print(add(1, 2))
+print(sub(1, 2))
+print(mul(1, 2))
+print(div(1, 2))
+print(mod(1, 2))
