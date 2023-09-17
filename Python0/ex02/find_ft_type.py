da71f1f27662a5b18abb37251ccbfe749d741aec
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-17 17:31:01 +0000

    ex02

--- Python0/ex02/find_ft_type.py
+++ Python0/ex02/find_ft_type.py
@@ -2,7 +2,7 @@ def all_thing_is_obj(object: any) -> int:
     if isinstance(object, int) is False and isinstance(object, str) is False:
         print(type(object).__name__.capitalize(), ":", type(object))
     elif isinstance(object, str):
-        print("Brian is in the kitchen :", type(object))
+        print(f"{object} is in the kitchen :", type(object))
     elif isinstance(object, int):
         print("Type not found")
     return 42
