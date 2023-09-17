7213eeb9f9ecf2e3a358a49d6efc79eb93d755ee
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-17 17:41:01 +0000

    ex06

--- Python0/ex06/ft_filter.py
+++ Python0/ex06/ft_filter.py
@@ -4,4 +4,6 @@ def ft_filter(function, iterable) -> list:
         for which function(item) is true.
         If function is None, return the items that are true.
     """
+    if function is None:
+        return iterable
     return [x for x in iterable if function(x)]
