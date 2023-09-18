a8177225e8b8c6e13e942cc62e83e2345797d0ee
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2023-09-18 14:54:36 +0000

    !

--- Python0/ex00/Hello.py
+++ Python0/ex00/Hello.py
@@ -5,11 +5,11 @@ ft_dict = {"Hello": "titi!"}
 
 ft_list[1] = "World!"
 ft_tuple = list(ft_list)
-ft_tuple[1] = "Malaysia"
+ft_tuple[1] = "Malaysia!"
 ft_tuple = tuple(ft_tuple)
 ft_set.remove("tutu!")
-ft_set.add("Subang Jaya")
-ft_dict["Hello"] = "42 Kuala Lumpur"
+ft_set.add("Subang Jaya!")
+ft_dict["Hello"] = "42 Kuala Lumpur!"
 
 print(ft_list)
 print(ft_tuple)
